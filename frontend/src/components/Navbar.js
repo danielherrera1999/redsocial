6d3db38ba5464d66b30e270a0e6153bbe0e6e9b1
Joey <joey523-@hotmail.com>
2019-10-13 19:52:33 -0500

    Frontend_edit
    joey hizo  la parte de la navegacion

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,61 @@
-import React , {Component} from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBContainer, MDBFormInline
+} from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export default class Navbar extends Component{
+export default class Navbar extends Component {
+    state = {
+        isOpen: false
+    };
 
-    render(){
+    toggleCollapse = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+    render() {
 
-        return(
-            <React.Fragment>
-                <ul>
-                    <li><Link to="/">login</Link></li>
-                    <li><Link to="/users">Usuarios</Link></li>
-                </ul>
-            </React.Fragment>
+        return (
+            <Router>
+                <MDBNavbar color="green" dark expand="md">
+                    <MDBNavbarBrand>
+                        <strong className="white-text">Navbar</strong>
+                    </MDBNavbarBrand>
+                    <MDBNavbarToggler onClick={this.toggleCollapse} />
+                    <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                        <MDBNavbarNav left>
+                            <MDBNavItem active>
+                                <MDBNavLink to="#!"><MDBIcon icon="home" size="1x" className="white-text pr-1" />Inicio</MDBNavLink>
+                            </MDBNavItem>
+                        </MDBNavbarNav>
+                        <MDBNavbarNav right>
+                        <MDBNavItem>
+                                <MDBFormInline waves>
+                                    <div className="md-form my-0">
+                                        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                                    </div>
+                                </MDBFormInline>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink className="waves-effect waves-light" to="#!">
+                                <i class="fas fa-user-plus"></i> 
+                                </MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink className="waves-effect waves-light" to="#!">
+                                <i class="fas fa-comments"></i>
+                                </MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink className="waves-effect waves-light" to="#!">
+                                    <i class="fas fa-bell"></i>
+                                </MDBNavLink>
+                            </MDBNavItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBNavbar>
+            </Router>
         );
     }
 } 
